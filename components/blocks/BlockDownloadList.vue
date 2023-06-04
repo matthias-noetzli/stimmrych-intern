35249903a4638ffdb303ea7a65d7caafbf7cc299
Matthias Nötzli <91967281+matthias-noetzli@users.noreply.github.com>
2023-06-04 11:48:17 +0200

    Make download lists accordions

--- components/blocks/BlockDownloadList.vue
+++ components/blocks/BlockDownloadList.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="block-download-list px-8 my-4 text-st-blue">
-    <h2 class="text-2xl">{{ block.title }}</h2>
-    <ul v-if="block.files" class="mt-2 grid gap-y-1">
-      <li v-for="file in block.files" :key="file.id">
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          :href="file.filename"
-          class="text-st-link hover:underline"
-          >{{ file.title }}</a
-        >
-      </li>
-    </ul>
+    <details>
+      <summary class="cursor-pointer group">
+        <h2 class="inline group-hover:underline">{{ block.title }}</h2>
+      </summary>
+      <ul v-if="block.files" class="mt-2 grid gap-y-1 pl-4">
+        <li v-for="file in block.files" :key="file.id">
+          <a target="_blank" rel="noopener noreferrer" :href="file.filename" class="text-st-link hover:underline">{{
+            file.title }}</a>
+        </li>
+      </ul>
+    </details>
   </div>
 </template>
 
